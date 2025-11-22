cb0b20dae14ecf384098a57470aea896f3b7690a
CoedydelaCruz <YOUR_GITHUB_EMAIL>
2025-11-22 18:58:24 +0800

    Uploaded

---
+++ registrations/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def registration(request):
+    return HttpResponse('IMANG KANOR!')
+
+def student(request):
+    return HttpResponse('DADA!')
+
+def student(request):
+    return HttpResponse('mama!')
+    
+
+    
+
+
+
+# Create your views here.
+
---
+++ student/views.py
@@ -0,0 +1,15 @@
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# def student(request):
+#     return HttpResponse('IMANG KANOR!')
+    
+    
+
+
+
+# Create your views here.
+
+
+# Create your views here.
